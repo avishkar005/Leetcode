7d8017add8dfa0785cb3d8c7e78297ebf1c8a6c3
avishkar005 <avishkarchoundkar4@gmail.com>
2025-08-12 23:22:36 +0530

    Create splitArray.java

---
+++ splitArray.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int low = 0, high = 0;
+        for (int num : nums) {
+            low = Math.max(low, num); 
+            high += num;              
+        }
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (canSplit(nums, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1; 
+            }
+        }
+        return low;
+    }
+
+    private boolean canSplit(int[] nums, int k, int maxSumAllowed) {
+        int currentSum = 0;
+        int count = 1; 
+        for (int num : nums) {
+            if (currentSum + num > maxSumAllowed) {
+                count++;
+                currentSum = num;
+                if (count > k) return false;
+            } else {
+                currentSum += num;
+            }
+        }
+        return true;
+    }
+}
